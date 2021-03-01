1049e1404d48f8992d29e88cb14d4f8a809ae99c
ljma <ljmamoreira@gmail.com>
2021-03-01 11:07:04 +0000

    Cleaned code; added docstrings.

--- src/nevh.py
+++ src/nevh.py
@@ -8,25 +8,64 @@
 
 import numpy as np
 
+# Hgrad: Hamiltonian grandient. 
 class Hgrad():
+    """
+    A class to compute the hamiltonian gradient of hamiltonian funcs.
+
+    Atributes
+    ---------
+    H: function H(t, [q_1,...,q_N, p_1,...,p_N])
+        Hamiltonian of physical system
+    N: int
+        number of degrees of freedom
+    dpsi: array[2N] of floats
+        step sizes for partial derivatives of hamiltonian
+    hparams: array[] of floats
+        parameters of the hamiltonian function
+    I: array[2N, 2N]
+        2Nx2N identity matrix
+
+    Methods
+    -------
+    __call__(t, np.array([q_1,...q_N, p_1,...p_N])
+        Returns a 2N np.array with the components of the hamiltonian gradient
+        [dH/dp_1,...,dH/dp_N, -dH/dq_1,...,-dH/dq_N]
+
+    """
+
     def __init__(self, H, dpsi, **hparams):
+        """
+        Creates and inits Hgrad objects
+
+        Parameters
+        ----------
+        H: function H(t, [q_1,...,q_N, p_1,...,p_N])
+            Hamiltonian of physical system
+        dpsi: array[2N] of floats
+            step sizes for partial derivatives of hamiltonian
+        hparams: array[] of floats
+            parameters of the hamiltonian function
+
+        """
+        # System global parameters  
         self.H = H
-        self.N = len(dpsi)
-        self.ndf = self.N // 2
-        self.dpsi = dpsi
-        self.hparams = hparams
-        self.I = np.eye(self.N)
+        self.N = len(dpsi) // 2     # Number of degrees of freedom
+        self.dpsi = dpsi            # Step sizes for differentiation
+        self.hparams = hparams      # Hamiltonian parameters
+        self.I = np.eye(2*self.N)   # Identity matrix
 
     def __call__(self, t, psi):
+        # Phase space gradient 
         delta_H = np.array(
             [self.H(t, psi + self.dpsi[i]*self.I[i]/2, **self.hparams)-
              self.H(t, psi - self.dpsi[i]*self.I[i]/2, **self.hparams)
-                 for i in range(self.N)])
+                 for i in range(2*self.N)])
         grad = delta_H / self.dpsi
-        hgrad = np.zeros(self.N)
-        ndf = self.N // 2
-        hgrad[:self.ndf] = grad[self.ndf:]
-        hgrad[self.ndf:] = -grad[:self.ndf]
+        # Reordering and signs
+        hgrad = np.zeros(2*self.N)
+        hgrad[:self.N] = grad[self.N:]
+        hgrad[self.N:] = -grad[:self.N]
         return hgrad
 
 
